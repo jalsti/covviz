c7ea93e300103cfe2644b29778e1e041094ab1ce
jalsti <57985357+jalsti@users.noreply.github.com>
2020-10-11 05:39:11 +0200

    timeseries plotting with better scaled 7 days sum plotting through using additional, 3rd, y axis; 7 day moving average removed to not overwhelm the eyes; grid for both main y-axes should hopefully be on same line; minor grid now for sums graph

--- src/dataPlotting.py
+++ src/dataPlotting.py
@@ -31,27 +31,31 @@ weeklyIncidenceLimit2Per100k = 50
 
 def plot_timeseries(datacolumns, dates, daily, cumulative, title, filename, ifShow=True, ifCleanup=True, population=None):
     """Creates the image with the different statistic graph plots for the covid-19 cases of a country, Bundesland or Kreis"""
+    from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, VPacker
 
     PLOT_YLIM_ENLARGER = 1.1
+    COLOR_INC_SUM = '#000060'
     fig, ax = plt.subplots(figsize=(10, 6))  # , constrained_layout=True)
+    
+    ax2 = ax.twinx()
+    ax_sum = ax.twinx()
+    
     # plt.tight_layout()
 
     # set grids
-    plt.grid(True, which='major', axis='x', ls='-', alpha=0.9)  # if not set here above, major ticks of x axis won't be visible
-    plt.grid(True, which='minor', axis='x', ls='--', alpha=0.7)  # if not set here above, minor ticks of x axis won't be visible
-    plt.grid(True, which='major', axis='y', ls='-', alpha=0.9)  # if not set here above, ticks of left side y axis won't be visible
-    plt.grid(True, which='minor', axis='y', ls='--', alpha=0.7)
+    ax.grid(True, which='major', axis='x', ls='-', alpha=0.9)  # if not set here above, major ticks of x axis won't be visible
+    ax.grid(True, which='minor', axis='x', ls='--', alpha=0.5)  # if not set here above, minor ticks of x axis won't be visible
+    #ax.grid(True, which='major', axis='y', ls='-', alpha=0.7)  # if not set here above, ticks of left side y axis won't be visible
+    #ax.grid(True, which='minor', axis='y', ls='--', alpha=0.5)
 
     # set x axis minor tick interval to only each 5 days one tick, as compromise between being exact and easily readable
-    ax.xaxis_date()
     fig.autofmt_xdate(rotation=60)
+    ax.xaxis_date()
     ax.xaxis.set_minor_locator(matplotlib.dates.DayLocator(bymonthday=range(1, 30, 5)))
 
 
     # plot raw daily cases
-    y_max = max(daily[1:])
-    plt.ylim(0, y_max * PLOT_YLIM_ENLARGER)
-    lns1 = ax.plot(dates, daily, label="raw daily cases (weekend-flawed)", color='#AAAAAA')
+    lns1 = ax.plot(dates, daily, label="raw daily cases (weekend-flawed)", color='#999999')
     # lns1 = ax.plot(dates, daily, label="daily cases (weekend-flawed), 2 weeks: red", color='#AAAAAA')
     # lns1_2 = ax.plot(dates[-14:], daily[-14:], label="daily cases, last 14 days dark gray", color='red')
     # print (len(dates[-14:]))
@@ -59,60 +63,117 @@ def plot_timeseries(datacolumns, dates, daily, cumulative, title, filename, ifSh
     # set y1 axis tick automatic interval and range restrictions, allow no 'half daily cases' (no floating point numbers)
     yloc = matplotlib.ticker.MaxNLocator(integer=True)
     ax.yaxis.set_major_locator(yloc)
-    y_max = max(daily[1:])
-    plt.ylim(0, y_max * PLOT_YLIM_ENLARGER)
-    plt.ylabel("daily cases")
-
-
-    # plot rolling sum of prior 7 days cases for date
-    lns0 = []
-    rolling_sum_max = 0
-    if population:
-        # plot 7 day sums, only if incidence borders are available
-        window = 7
-        rolling_sum = pandas.DataFrame(daily).rolling(window=window, center=False).sum()
-        lns0 = ax.plot(dates, rolling_sum, label='sum of daily cases, for prior %s days of date' % window, color='#000060')
-        rolling_sum_max = rolling_sum[0].max()
-        y_max = max(y_max, rolling_sum_max)
-        plt.ylim(0, y_max * PLOT_YLIM_ENLARGER)
-
+    ax.set_ylim(0, max(daily[1:]) * PLOT_YLIM_ENLARGER)
 
     #  plot rolling averages for daily cases
     window = 14
     rolling_mean = pandas.DataFrame(daily).rolling(window=window, center=True).mean()
-    lns3 = ax.plot(dates, rolling_mean, label="centered moving average, %s days' cases" % window, color='orange', linewidth=4)
-    window = 7
-    rolling_mean = pandas.DataFrame(daily).rolling(window=window, center=True).mean()
-    lns2 = ax.plot(dates, rolling_mean, label="centered moving average, %s days' cases" % window, color='#900090')
+    lns3 = ax.plot(dates, rolling_mean, label="centered moving average, %s days cases" % window, color='orange', linewidth=4)
+    lns2 = []
+    # window = 7
+    # rolling_mean = pandas.DataFrame(daily).rolling(window=window, center=True).mean()
+    # lns2 = ax.plot(dates, rolling_mean, label="centered moving average, %s days' cases" % window, color='#900090')
     # window=21
     # rolling_mean = pandas.DataFrame(daily).rolling(window=window, center=True).mean()
     # ax.plot(dates, rolling_mean, label='SMA %s days' % window, color='pink', linewidth=1)
 
+    # y-axis label multi colored
+    ybox1 = TextArea("raw daily cases", textprops=dict(color=lns1[0].get_color(), rotation='vertical'))
+    ybox2 = TextArea(" / ", textprops=dict(color="black", rotation='vertical'))
+    ybox3 = TextArea("moving average", textprops=dict(color=lns3[0].get_color(), rotation='vertical'))
+    ybox = VPacker(children=[ybox3, ybox2, ybox1], align="center", pad=0, sep=5)
+    anchored_ybox = AnchoredOffsetbox(loc=8, child=ybox, pad=0., frameon=False,
+                                      bbox_to_anchor=(-0.06, 0.2),
+                                      bbox_transform=ax.transAxes, borderpad=0.)
+    ax.add_artist(anchored_ybox)
 
-    # plot incidence border lines
+
+    # plot rolling sum of prior 7 days cases for date
+    rolling_sum_max = 0
+    lns0 = []
     lns6_1 = []
     lns6_2 = []
     if population:
+        ax_sum.grid(True, which='major', axis='y', ls='-', alpha=0.6, color=COLOR_INC_SUM)  # if not set here above, ticks of left side y axis won't be visible
+        ax_sum.grid(True, which='minor', axis='y', ls='--', alpha=0.1, color=COLOR_INC_SUM)
+        
+        # move total cases y axis to outside
+        ax2.spines["right"].set_position(("axes", 1.15))
+        yloc2 = matplotlib.ticker.MaxNLocator(integer=True)
+        ax_sum.yaxis.set_major_locator(yloc2)
+
+        # plot 7 day sums, only if incidence borders are available
+        window = 7
+        label = 'sum of daily cases, for prior %s days of date' % window
+        rolling_sum = pandas.DataFrame(daily).rolling(window=window, center=False).sum()
+        # plot yellow background
+        lns0 = ax_sum.plot(dates, rolling_sum, label=label, color='yellow', linewidth=7, alpha=0.4)
+        lns0 = ax_sum.plot(dates, rolling_sum, label=label, color=COLOR_INC_SUM)
+        rolling_sum_max = rolling_sum[0].max()
+        
+        # set axis properties
+        ax_sum.set_ylim(0, rolling_sum_max * PLOT_YLIM_ENLARGER)
+        # also yellow background for label
+        ax_sum.set_ylabel(label + "\n(to determine incidence borders)", color=COLOR_INC_SUM, 
+                          bbox=dict(color='yellow', alpha=0.3, boxstyle='round', mutation_aspect=0.5))
+        ax_sum.tick_params(axis='y', colors=COLOR_INC_SUM, size=4, width=1.5)
+        
+        # adjust axis visibility
+        ax_sum.set_frame_on(True)
+        ax_sum.patch.set_visible(False)
+        for sp in ax_sum.spines.values():
+            sp.set_visible(False)
+        ax_sum.spines["right"].set_visible(True)
+        
+        
+        yticks = yloc2()
+        ydiff = yticks[1]
+        yminor = int(ydiff / 5 ) if ydiff >= 8 else 1  
+        # print(f"{ydiff} results in {yminor} for {title} and {yticks=}")
+        ax_sum.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(yminor))
+
+
+    
+        # plot incidence border lines
         limit = weeklyIncidenceLimit1Per100k * population / 100000
         # print ("limit:", limit)
-        lns6_1 = ax.plot([dates[0]] + [dates[-3]], [limit, limit], label="incid. border %i/week/100k pop.: %.2f" % (weeklyIncidenceLimit1Per100k, limit), color='#ff8c8c', linestyle=(0, (3, 5)))
+        lns6_1 = ax_sum.plot([dates[40]] + [dates[-1]], [limit, limit], label="incid. border %i/week/100k pop.: %.2f" % (weeklyIncidenceLimit1Per100k, limit), color='#ff8c8c', linestyle=(0, (3, 5)))
 
         # plot second incidence border only if first one is nearly reached, to have no unneeded large y1 numbers which would worsen the view
         if rolling_sum_max > limit * 0.8:
             limit = weeklyIncidenceLimit2Per100k * population / 100000
             # print ("limit:", limit)
-            lns6_2 = ax.plot([dates[0]] + [dates[-3]], [limit, limit], label="incid. border %i/week/100k pop.: %.2f" % (weeklyIncidenceLimit2Per100k, limit), color='#df4c4c', linestyle=(0, (5, 4)))
+            lns6_2 = ax_sum.plot([dates[40]] + [dates[-1]], [limit, limit], label="incid. border %i/week/100k pop.: %.2f" % (weeklyIncidenceLimit2Per100k, limit), color='#df4c4c', linestyle=(0, (5, 4)))
+        ax_sum.set_ylim(0, max(rolling_sum_max, limit) * PLOT_YLIM_ENLARGER)
+        
+        # set new ax limit for daily cases / averaging, trying to have evenly distributed major ticks for both
+        
+        #yticks_daily = len(yloc())
+        #yticks_sum = len(yloc2())
+        #ystep_daily = 0
+        #if (tick_diff := yticks_sum - yticks_daily) > 0:
+        #    ystep_daily = yloc()[1]
+        #    ax.set_ylim(0, ystep_daily * yticks_sum * PLOT_YLIM_ENLARGER)
+        #print(yticks_daily, yticks_sum, tick_diff, ystep_daily, ystep_daily * yticks_sum * PLOT_YLIM_ENLARGER)
+        #print(yloc(), yloc2())
+        ax.set_ylim(0, yloc()[-2])
+        ax_sum.set_ylim(0, yloc2()[-2])
+        yticks = yloc2()
+        ydiff = yticks[1]
+        yminor = int(ydiff / 5 + 0.5) if ydiff >= 8 else 1  # '+0.5' to round up for '8'
+        # print(f"{ydiff} results in {yminor} for {title} and {yticks=}")
+        ax_sum.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(yminor))
+        
 
-        y_max = max(y_max, limit)
-        plt.ylim(0, y_max * PLOT_YLIM_ENLARGER)
-
-
-    # determine some meaningfull y1 minor tick value, also used for setting center bar's y position
-    yticks = yloc()
-    ydiff = yticks[1]
-    yminor = int(ydiff / 5 + 0.5) if ydiff >= 8 else 1  # '+0.5' to round up for '8'
-    # print(f"{ydiff} results in {yminor} for {title} and {yticks=}")
-    ax.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(yminor))
+    if not population:
+        ax.grid(True, which='major', axis='y', ls='-', alpha=0.9)  # if not set here above, ticks of left side y axis won't be visible
+        ax.grid(True, which='minor', axis='y', ls='--', alpha=0.7)
+        # determine some meaningfull y1 minor tick value, also used for setting center bar's y position
+        yticks = yloc()
+        ydiff = yticks[1]
+        yminor = int(ydiff / 5 + 0.5) if ydiff >= 8 else 1  # '+0.5' to round up for '8'
+        # print(f"{ydiff} results in {yminor} for {title} and {yticks=}")
+        ax.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(yminor))
 
 
     # plot temporal center date marker
@@ -133,11 +194,14 @@ def plot_timeseries(datacolumns, dates, daily, cumulative, title, filename, ifSh
 
 
     # plot cumulative cases data for the 2nd y axis
-    ax2 = plt.twinx()
-    plt.ylim(0, max(cumulative) * 1.1)
-    lns5 = ax2.plot(dates, cumulative, label="total cases reported at RiskLayer", color='#1E90FF', linestyle='dotted', linewidth=2)
-    plt.ylabel("cumulative total cases", color="#1E90FF")
+    lns5 = ax2.plot(dates, cumulative, label="total cases reported at RiskLayer", color='#1E90FF', 
+                    linestyle='dotted', linewidth=2)
+    ax2.set_ylim(0, max(cumulative) * PLOT_YLIM_ENLARGER)
+    ax2.set_ylabel("cumulative total cases")
+    ax2.yaxis.label.set_color(color=lns5[0].get_color())
+    ax2.tick_params(axis='y', colors=lns5[0].get_color())
 
+        
     # build legend
     # collect lines which shall get a label in legend
     lines = lns4_1 + lns5 + lns0 + lns1 + lns2 + lns3  + lns6_2 + lns6_1
@@ -157,6 +221,7 @@ def plot_timeseries(datacolumns, dates, daily, cumulative, title, filename, ifSh
     ax.xaxis.set_major_locator(matplotlib.dates.DayLocator(bymonthday=range(1, 32, 31)))  # if placing this setting above all other, major ticks won't appear
     ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%Y-%m"))  # its date formatter must be set, if setting major locator
 
+    
     if filename:
         fig.savefig(os.path.join(dataFiles.PICS_PATH, filename), bbox_inches='tight')
 
@@ -168,6 +233,7 @@ def plot_timeseries(datacolumns, dates, daily, cumulative, title, filename, ifSh
         plt.close("all")
 
     return plt, fig, ax, ax2
+
 
 
 def test_plot_Kreis(ts, bnn, dates, datacolumns):
